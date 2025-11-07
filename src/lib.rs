17a7c73f8fd17c7da5c72b150b568b42382aca16
TheEros <loveuschina@gmail.com>
2025-11-07 14:08:20 +0800

    init code commit

---
+++ src/lib.rs
@@ -0,0 +1,31 @@
+//! src/lib.rs
+use actix_web::dev::Server;
+use actix_web::{App, HttpResponse, HttpServer, web};
+use std::net::TcpListener;
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    name: String,
+    email: String,
+}
+
+pub async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+// Notice the different signature!
+// We return `Server` on the happy path and we dropped the `async` keyword
+// We have no .await call, so it is not needed anymore.
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
+    // No .await here!
+    Ok(server)
+}
